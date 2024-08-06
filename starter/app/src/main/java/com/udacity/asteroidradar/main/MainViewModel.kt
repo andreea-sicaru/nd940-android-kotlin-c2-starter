2b59b022f0d8af228df7c5de6123a91571ab7adf
Andreea Sicaru <andreea.sicaru@uber.com>
2024-08-06 15:37:51 +0300

    Simplify picture of the day logic

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.udacity.asteroidradar.main
 
 import android.app.Application
+import android.net.ConnectivityManager
+import android.util.Log
+import androidx.core.content.getSystemService
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,19 +11,15 @@ import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.api.NasaApi
 import com.udacity.asteroidradar.database.getDatabase
 import com.udacity.asteroidradar.model.Asteroid
+import com.udacity.asteroidradar.model.PictureOfDay
 import com.udacity.asteroidradar.repository.AsteroidsRepository
 import kotlinx.coroutines.launch
 
-class MainViewModel(application: Application) : ViewModel() {
+class MainViewModel(private val application: Application) : ViewModel() {
 
-    private val _pictureOfTheDayUrl = MutableLiveData<String?>()
-    val pictureOfTheDayUrl: LiveData<String?>
-        get() = _pictureOfTheDayUrl
-
-    private val _pictureOfTheDayDescription = MutableLiveData<String?>()
-    val pictureOfTheDayDescription: LiveData<String?>
-        get() = _pictureOfTheDayDescription
-
+    private val _pictureOfTheDay = MutableLiveData<PictureOfDay>()
+    val pictureOfTheDay: LiveData<PictureOfDay>
+        get() = _pictureOfTheDay
 
     private val _navigateToSelectedAsteroid = MutableLiveData<Asteroid?>()
     val navigateToSelectedAsteroid: LiveData<Asteroid?>
@@ -44,10 +43,18 @@ class MainViewModel(application: Application) : ViewModel() {
 
     private fun getPictureOfTheDay() {
         viewModelScope.launch {
-            val pictureOfTheDay = NasaApi.service.getPictureOfDayAsync().await()
-            if (pictureOfTheDay.mediaType == "image") {
-                _pictureOfTheDayUrl.value = pictureOfTheDay.url
-                _pictureOfTheDayDescription.value = pictureOfTheDay.title
+            val connectivityManager =
+                application.applicationContext.getSystemService<ConnectivityManager>()
+            val activeNetwork = connectivityManager?.activeNetworkInfo
+            val isConnected = activeNetwork?.isConnectedOrConnecting == true
+
+            if (isConnected) {
+                val pictureOfTheDay = NasaApi.service.getPictureOfDayAsync().await()
+                if (pictureOfTheDay.mediaType == "image") {
+                    _pictureOfTheDay.value = pictureOfTheDay
+                }
+            } else {
+                Log.d("AsteroidsRepository", "No internet connection.")
             }
         }
     }
